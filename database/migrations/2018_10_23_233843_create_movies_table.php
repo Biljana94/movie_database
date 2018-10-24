1bc121aeacfe45bb522b07d01c7fa0ad08b582ed
Biljana Jelaca <bilja.jelaca@gmail.com>
2018-10-24 02:19:06 +0200

    Add migration create_movies_table

---
+++ database/migrations/2018_10_23_233843_create_movies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title'); //naslov filma
+            $table->string('genre'); //zanr
+            $table->string('director'); //reziser
+            $table->string('year'); //godina snimanja
+            $table->text('storyline'); //opis (zaplet)
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
